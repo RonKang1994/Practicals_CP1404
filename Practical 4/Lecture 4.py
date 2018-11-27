8b6e92a6098371f8ed288b69506b63ac76cca972
Ron Kang <ronjinhong.kang@my.jcu.edu.au>
2018-11-27 08:42:54 +0800

    Commit changes -Practical 4, lecture codes - created long_list function: The function takes in a string converts it to a list that consists of only those which has a character count of more than a certain number

--- Practical 4/Lecture 4.py
+++ Practical 4/Lecture 4.py
@@ -13,8 +13,22 @@ def check_vowel():
     print("Out of {} letters {} has {} vowels".format(letter, name, vowel))
 
 
+def long_list(text):
+    text_list = text.split()
+    final_list = list()
+    for i in range(0, len(text_list)):
+        if len(text_list[i]) > 3:
+            final_list.append(text_list[i])
+    print(final_list)
+
+
 def main():
-    check_vowel()
+    # Give a name and check how many vowels there are in it
+    # check_vowel()
+
+    # Convert a string into a list and give ones which have 3> characters
+    text = "This is a sentence"
+    long_list(text)
 
 
 main()

78e85f921a808540e120ad5d935fb9908816744a
Ron Kang <ronjinhong.kang@my.jcu.edu.au>
2018-11-27 11:51:37 +0800

    Commit changes -Practical 4, lotter_ticket_generator.py added and done

--- Practical 4/loterry_ticket_generator.py
+++ Practical 4/loterry_ticket_generator.py
@@ -1,22 +1,15 @@
 import random
 
 def main():
-    pick = int(input("How many quick picks? "))
-    for i in range(pick):
+    pick_total = int(input("How many quick picks? "))
+    for i in range(pick_total):
         picks = []
         for j in range(6):
-            picks.append(random.randrange(46))
+            pick = random.randrange(46)
+            while pick in picks:
+                pick = random.randrange(46)
+            picks.append(pick)
             if j == 5:
-                while(picks[0] == picks[1] or picks[0] == picks[2] or picks[0] == picks[3] or picks[0] == picks[4] or picks[0] == picks[5]):
-                    picks[0] = random.randrange(46)
-                while(picks[1] == picks[2] or picks[1] == picks[3] or picks[1] == picks[4] or picks[1] == picks[5]):
-                    picks[1] = random.randrange(46)
-                while(picks[2] == picks[3] or picks[2] == picks[4] or picks[2] == picks[5]):
-                    picks[2] = random.randrange(46)
-                while (picks[3] == picks[4] or picks[3] == picks[5]):
-                    picks[3] = random.randrange(46)
-                while (picks[4] == picks[5]):
-                    picks[3] = random.randrange(46)
                 picks.sort()
                 print(picks)
 

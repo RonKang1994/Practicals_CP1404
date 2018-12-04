0fbfe0c815de573a4c2ad3cd193c3f0e5153c8ba
Ron Kang <ronjinhong.kang@my.jcu.edu.au>
2018-12-04 10:11:29 +0800

    Commit changes -Practical 5, color_names.py comments

--- Practical 5/color_names.py
+++ Practical 5/color_names.py
@@ -3,11 +3,12 @@ def main():
     CP1404/CP5632 Practical
     Color names in a dictionary
     """
+    # Color dictionary
     COLOR_NAMES = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "aquamarine": "#7fffd4",
                    "azure": "#f0ffff", "beige": "#f5f5dc", "bisque": "#ffe4c4", "black": "#000000",
                    "blueviolet": "#8a2be2", "brown	": "#a52a2a", "chocolate": "#d2691e"}
 
-    color = input("Enter color: ")
+    color = input("Enter color: "}
     while color != "":
         if color.lower() in COLOR_NAMES:
             print(color.lower(), "color code is", COLOR_NAMES[color.lower()])
